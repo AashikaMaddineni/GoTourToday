6e01ae2a1205c815f49fc3e0ffae810d58c01c02
AashikaMaddineni <67450233+AashikaMaddineni@users.noreply.github.com>
2021-05-19 20:33:47 +0530

    # Added SplashScreenActivity

---
+++ app/src/main/java/com/aashika/gotourtoday/SplashScreenActivity.java
@@ -0,0 +1,35 @@
+package com.aashika.gotourtoday;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import androidx.annotation.Nullable;
+
+
+public class SplashScreenActivity extends Activity {
+    long Delay = 8000;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash_screen);
+        ImageView image = (ImageView) findViewById(R.id.LogoImage);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.together);
+        image.startAnimation(animation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                finish();
+            }
+        },4000);
+    }
+}
